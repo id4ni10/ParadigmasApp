a5f1e1ad8d6878f74a6f3d4f30966f5023f863f5
hgsaldanha <hgsaldanha@gmail.com>
2014-07-15 21:46:22 -0300

    Create Blog.cs

---
+++ ParadigmasApp/App_Code/Blog.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+//Nosso Assembly
+using Frame.Core;
+/// <summary>
+/// Classe teste de implementação cliente
+/// </summary>
+/// 
+public class Blog : GenericController
+{
+    private List<Post> posts = new List<Post>();
+
+    public Blog()
+    {
+        //posts.Add(new Post("Aziz", "Sault"));
+        //posts.Add(new Post("Hernã", "Teste"));
+        //posts.Add(new Post("Aquiles", "Mizerê"));
+    }
+
+    public override object GET(Dictionary<String,Object> param)
+    {
+        if (Session["obj"] != null)
+        {
+            posts = (List<Post>)Session["obj"];
+        }
+        return posts;
+    }
+
+    public override object POST(Dictionary<String, Object> param)
+    {
+        String titulo = (String)param["titulo"];
+        String conteudo = (String)param["conteudo"];
+        if (Session["obj"] != null)
+        {
+            posts = (List<Post>)Session["obj"];
+        }
+
+        posts.Add(new Post(titulo,conteudo));
+        return posts;
+    }
+
+    public override object PUT(Dictionary<String, Object> param)
+    {
+        return "PUT";
+    }
+
+    public override object DELETE(Dictionary<String, Object> param)
+    {
+        return "DELETE";
+    }
+}
